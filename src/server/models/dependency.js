46acb813430f6697c37cb4195ebbad71d6d682c5
thenengah <thenengah@gmail.com>
2017-11-06 20:05:37 +0800

    added try/catch to worker
    src/server/models/dependency.js
    - removed try/catch on api calls
    
    src/server/worker.js
    - added try/catch

--- src/server/models/dependency.js
+++ src/server/models/dependency.js
@@ -36,47 +36,31 @@ class Dependency {
   }
 
   async fromCache() {
-    try {
-      const value = await redis.run('get', this.key) 
-      if (value) this.dependencies = JSON.parse(value).dependencies
-    } catch (er) { console.log(er.stack) }
+    const value = await redis.run('get', this.key) 
+    if (value) this.dependencies = JSON.parse(value).dependencies
     return this.props.dependencies ? this.props : null
   }
 
   async fromRegistry() {
-    try {
-      const repo = await this.getRepo()
-      const pack = await this.getPack(repo)
-      const raw = await this.getRaw(pack)
-      this.dependencies = extract(raw) 
-      await redis.run('set', this.key, [JSON.stringify(this.props)])
-      await redis.run('expire', this.key, [3600])
-    } catch (er) { console.log(er.stack) }
+    const repo = await this.getRepo()
+    const pack = await this.getPack(repo)
+    const raw = await this.getRaw(pack)
+    this.dependencies = extract(raw) 
+    await redis.run('set', this.key, [JSON.stringify(this.props)])
+    await redis.run('expire', this.key, [3600])
     return this.props
   }
 
   async getRepo() {
-    try {
-      return (await get({ uri: uris.repo(this.name) })).match(re.repo)[1]
-    } catch (er) {
-      console.log(`field to get repo ${this.name}`)
-    }
+    return (await get({ uri: uris.repo(this.name) })).match(re.repo)[1]
   }
 
   async getPack(repo) {
-    try {
-      return (await get({ uri: uris.file(repo) })).match(re.file)[1]
-    } catch (er) {
-      console.log(`field to get pack ${repo}`)
-    }
+    return (await get({ uri: uris.file(repo) })).match(re.file)[1]
   }
 
   async getRaw(pack) {
-    try {
-      return await get({ uri: uris.raw(pack), json: true })
-    } catch (er) {
-      console.log(`field to get raw ${pack}`)
-    }
+    return await get({ uri: uris.raw(pack), json: true })
   }
 
 }
--- src/server/worker.js
+++ src/server/worker.js
@@ -1,16 +1,16 @@
-import semver from 'semver'
-
 import redis from './redis'
 import { Dependency } from './models'
 
 const work = async(set=[]) => { 
-  const name = set.shift()
-  if (name) {
-    console.log(await redis.run('zrevrank', 'dependencies', [name]), name)
-    await new Dependency(name).get()
-  }
-  set = set.length ? set : (await redis.run('zrevrange', 'dependencies', [0, 250]))
-  setTimeout(async() => { await work(set) }, 100)
+  try {
+    const name = set.shift()
+    if (name) {
+      console.log(await redis.run('zrevrank', 'dependencies', [name]), name)
+      await new Dependency(name).get()
+    }
+    set = set.length ? set : (await redis.run('zrevrange', 'dependencies', [0, 250]))
+  } catch (er) { console.log(er.stack) }
+  setTimeout(async() => { await work(set) }, 500)
 }
 
 export default {
